f4d901e06e5128c46c5bf1ddae2761ab0dc6ef4e
Анатолий Андреев <anatoliiandreev19@gmail.com>
2022-07-16 23:06:54 +0300

    first

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+         UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+         userDao.dropUsersTable();
+         userDao.createUsersTable();
+         userDao.saveUser("John","Mike", (byte) 31);
+         userDao.removeUserById(0);
+         userDao.getAllUsers();
+         userDao.cleanUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static Connection connection;
+    private static SessionFactory sessionFactory;
+
+    public static Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?&serverTimeZone=Europe/Moscow&useSSL=false&allowPublicKeyRetrieval=true","root","admin");
+        connection.setAutoCommit(false);
+        return connection;
+    }
+
+    public Util() throws SQLException {
+    }
 }
