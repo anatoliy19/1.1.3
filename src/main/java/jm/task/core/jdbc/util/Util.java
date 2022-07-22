26a0350d659676268542982036f3d695c0a141af
Анатолий Андреев <anatoliiandreev19@gmail.com>
2022-07-22 12:40:03 +0300

    22.07 connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,8 @@
 package jm.task.core.jdbc.util;
-
-import org.hibernate.SessionFactory;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Util {
-
-     public static Connection connection;
      private static final String Connection_Url = "jdbc:mysql://localhost:3306/test?" +
              "&serverTimeZone=Europe/Moscow&useSSL=false&allowPublicKeyRetrieval=true";
      private static final String User = "root";
@@ -15,11 +10,12 @@ public class Util {
 
     public static Connection getConnection() {
 
+        Connection connection = null;
+
         try
         {
-           Connection connection = DriverManager.getConnection( Connection_Url,User,Password);
+           connection = DriverManager.getConnection( Connection_Url,User,Password);
            connection.setAutoCommit(false);
-            return connection;
         } catch (Exception e) {
             e.printStackTrace();
         }
